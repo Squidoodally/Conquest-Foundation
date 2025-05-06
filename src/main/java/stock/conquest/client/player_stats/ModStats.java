c44e9ba4876ba492d494f3170e3ce2f05f198506
Squidoodally <harrysmales1@gmail.com>
2025-05-07 00:39:04 +1000

    Removed testing GUI Added player attributes stamina and mana.

--- src/main/java/stock/conquest/client/player_stats/ModStats.java
+++ src/main/java/stock/conquest/client/player_stats/ModStats.java
@@ -3,6 +3,7 @@ package stock.conquest.client.stat;
 import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import stock.conquest.component.ModComponents;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,21 +39,5 @@ public abstract class ModStats implements AutoSyncedComponent {
     public void setStat(String statKey, int value) {
         stats.put(statKey, value);
         ModComponents.PLAYER_STATS.sync(player);  // You may need custom sync logic here
-    }
-
-    @Override
-    public void readFromNbt(NbtCompound tag) {
-        for (String key : stats.keySet()) {
-            if (tag.contains(key)) {
-                stats.put(key, tag.getInt(key));
-            }
-        }
-    }
-
-    @Override
-    public void writeToNbt(NbtCompound tag) {
-        for (Map.Entry<String, Integer> entry : stats.entrySet()) {
-            tag.putInt(entry.getKey(), entry.getValue());
-        }
     }
 }
