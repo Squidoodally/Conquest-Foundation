44204fa4e8210e2c68189e862f62419d74b5f1f5
Squidoodally <harrysmales1@gmail.com>
2025-05-07 00:38:12 +1000

    Removed testing GUI Added player attributes stamina and mana.

--- src/main/java/stock/conquest/client/player_stats/ModStats.java
+++ src/main/java/stock/conquest/client/player_stats/ModStats.java
@@ -1,14 +1,14 @@
 package stock.conquest.client.player_stats;
 
-import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtCompound;
+import org.ladysnake.cca.api.v3.component.ComponentV3;
+import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
 import stock.conquest.component.ModComponents;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class ModStats implements AutoSyncedComponent {
+public abstract class ModStats implements ComponentV3, AutoSyncedComponent {
     private final PlayerEntity player;
     private final Map<String, Integer> stats = new HashMap<>();
 
@@ -38,22 +38,7 @@ public abstract class ModStats implements AutoSyncedComponent {
 
     public void setStat(String statKey, int value) {
         stats.put(statKey, value);
-        ModComponents.PLAYER_STATS.sync(player);  // You may need custom sync logic here
-    }
-
-    @Override
-    public void readFromNbt(NbtCompound tag) {
-        for (String key : stats.keySet()) {
-            if (tag.contains(key)) {
-                stats.put(key, tag.getInt(key));
-            }
-        }
-    }
-
-    @Override
-    public void writeToNbt(NbtCompound tag) {
-        for (Map.Entry<String, Integer> entry : stats.entrySet()) {
-            tag.putInt(entry.getKey(), entry.getValue());
-        }
+        // Automatically sync the component after a stat change.
+        ModComponents.PLAYER_STATS.sync(player);
     }
 }
